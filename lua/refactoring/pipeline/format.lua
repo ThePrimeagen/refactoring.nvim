ba3d221064817aa749915a9ca79f043e6c2a4ce7
ThePrimeagen <mpaulson@netflix.com>
2021-07-27 21:10:02 -0600

    fix: Annoying no format found

--- lua/refactoring/pipeline/format.lua
+++ lua/refactoring/pipeline/format.lua
@@ -1,8 +1,6 @@
 local function format(refactor)
     local format_cmd = refactor.options.formatting[refactor.lang].cmd
-    if not format_cmd then
-        print("No Format Command", vim.inspect(refactor.options))
-    else
+    if format_cmd then
         for _, bufnr in pairs(refactor.buffers) do
             -- TODO: Window?
             vim.api.nvim_win_set_buf(0, bufnr)
