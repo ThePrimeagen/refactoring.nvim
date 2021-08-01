72c5e1a5cf8a7acfbe362113a2941723258c55c5
teddylear <teddylear@gmail.com>
2021-07-31 22:08:15 -0400

    feat: Attempt to add golang support. Still need to find out why parameters are not working when query appears to be correct.

---
+++ queries/go/refactoring.scm
@@ -0,0 +1,16 @@
+;; Grabs all the local variable declarations.  This is useful for scope
+;; variable passing.  Which variables do we need to pass to the extracted
+;; function?
+((short_var_declaration
+    (expression_list
+    (identifier) @definition.local_var)))
+
+;; grabs all the arguments that are passed into the function.  Needed for
+;; function extraction, 106
+(function_declaration
+   parameters: (parameter_list
+   (parameter_declaration) @definition.function_argument))
+
+;; TODO is this scope required? Fails when this is uncommented
+;; (program) @definition.scope
+(function_declaration) @definition.scope
