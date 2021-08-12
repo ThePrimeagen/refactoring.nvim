1a6b596907dda8200846b8743d15ec2fed300af1
Andrea Cappuccio <hood@null.net>
2021-08-12 16:44:39 +0200

    feat: extract var for lua
    Closes #43

--- lua/refactoring/code_generation/lua.lua
+++ lua/refactoring/code_generation/lua.lua
@@ -1,29 +1,32 @@
-    local lua = {
-        extract_function = function(opts)
-            return {
-                create = string.format(
-                    [[
+local lua = {
+    create_constant = function(opts)
+        return string.format("local %s = %s\n", opts.name, opts.value)
+    end,
+    extract_function = function(opts)
+        return {
+            create = string.format(
+                [[
 local function %s(%s)
     %s
     return %s
 end
 
 ]],
-                    opts.name,
-                    table.concat(opts.args, ", "),
-                    type(opts.body) == "table"
-                            and table.concat(opts.body, "\n")
-                        or opts.body,
-                    opts.ret
-                ),
+                opts.name,
+                table.concat(opts.args, ", "),
+                type(opts.body) == "table"
+                        and table.concat(opts.body, "\n")
+                    or opts.body,
+                opts.ret
+            ),
 
-                call = string.format(
-                    "local %s = %s(%s)",
-                    opts.ret,
-                    opts.name,
-                    table.concat(opts.args, ", ")
-                ),
-            }
-        end,
-    }
-    return lua
+            call = string.format(
+                "local %s = %s(%s)",
+                opts.ret,
+                opts.name,
+                table.concat(opts.args, ", ")
+            ),
+        }
+    end,
+}
+return lua
