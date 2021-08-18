fd3168b8b62416337b6ee39d9fd0322b1227613e
teddylear <teddylear@gmail.com>
2021-08-17 21:11:25 -0400

    feat: add golang support for 119
    Noticed there is an issue running refactor_spec.lua alone due to nil
    scope. Looks related to something in tests as the failing test had no
    changes in this PR.

---
+++ lua/refactoring/tests/refactor/119/go/example/extract_var.expected.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Order struct {
+	quantity  float64
+	itemPrice float64
+}
+
+func orderCalculation(order Order, a int, b int) (total float64) {
+	temp := a * b
+basePrice := order.quantity*order.itemPrice
+blah := (basePrice) - 7
+	fmt.Println(blah, temp)
+	return basePrice -
+		math.Max(0, order.quantity-500)*order.itemPrice*0.05 +
+		math.Min(basePrice*0.1, 100)
+}
---
+++ lua/refactoring/tests/refactor/119/go/example/extract_var.start.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Order struct {
+	quantity  float64
+	itemPrice float64
+}
+
+func orderCalculation(order Order, a int, b int) (total float64) {
+	temp := a * b
+	blah := (order.quantity *
+		order.itemPrice) - 7
+	fmt.Println(blah, temp)
+	return order.quantity*order.itemPrice -
+		math.Max(0, order.quantity-500)*order.itemPrice*0.05 +
+		math.Min(order.quantity*
+			order.itemPrice*0.1, 100)
+}
