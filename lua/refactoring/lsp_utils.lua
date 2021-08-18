28ac35aee8ae238c8a5363931161f9aebee6b1b4
ThePrimeagen <mpaulson@netflix.com>
2021-08-18 12:17:07 -0600

    feat: text editing.  this should really solidify the refactoring

--- lua/refactoring/lsp_utils.lua
+++ lua/refactoring/lsp_utils.lua
@@ -16,6 +16,32 @@ function M.lsp_uri_to_bufnr(file)
     return vim.fn.bufnr(strip_file_uri(file))
 end
 
+function M.delete_text(region)
+    return { region:to_lsp_text_edit("") }
+end
+
+function M.insert_text(region, text)
+    local clone = region:clone()
+
+    -- TODO: I think this is a bug in neovim.
+    -- The reason being to successfully insert at this character, we have to
+    -- make the start != end col or else it consumes one character
+    --
+    -- LSP definition:
+    -- https://microsoft.github.io/language-server-protocol/specification#textEdit
+    clone.end_row = clone.start_row
+    clone.end_col = clone.start_col - 1
+
+    return { clone:to_lsp_text_edit(text) }
+end
+
+function M.replace_text(region, text)
+    local delete_text = M.delete_text(region)
+    local insert_text = M.insert_text(region, text)
+
+    return { insert_text[1], delete_text[1] }
+end
+
 function M.get_definition_under_cursor(bufnr)
     local def = vim.lsp.buf_request_sync(
         bufnr,
--- lua/refactoring/tests/region_spec.lua
+++ lua/refactoring/tests/region_spec.lua
@@ -2,28 +2,27 @@ local Region = require("refactoring.region")
 local eq = assert.are.same
 local vim_motion = require("refactoring.tests.utils").vim_motion
 
+local function setup()
+    vim.cmd(":new")
+    vim.api.nvim_buf_set_lines(0, 0, -1, false, {
+        "foo",
+        "if (true) {",
+        "    bar",
+        "}",
+    })
+end
+
 describe("Region", function()
     it("select text : line", function()
-        vim.cmd(":new")
-        vim.api.nvim_buf_set_lines(0, 0, -1, false, {
-            "foo",
-            "if (true) {",
-            "    bar",
-            "}",
-        })
+        setup()
+
         vim_motion("jV")
         local region = Region:from_current_selection()
         eq(region:get_text(), { "if (true) {" })
     end)
 
     it("select text : partial-line", function()
-        vim.cmd(":new")
-        vim.api.nvim_buf_set_lines(0, 0, -1, false, {
-            "foo",
-            "if (true) {",
-            "    bar",
-            "}",
-        })
+        setup()
 
         -- TODO: Why is first selection just not present...
         vim_motion("jwvww")
@@ -32,13 +31,7 @@ describe("Region", function()
     end)
 
     it("select text : multi-partial-line", function()
-        vim.cmd(":new")
-        vim.api.nvim_buf_set_lines(0, 0, -1, false, {
-            "foo",
-            "if (true) {",
-            "    bar",
-            "}",
-        })
+        setup()
 
         -- TODO: Why is first selection just not present...
         vim.cmd(":1")
