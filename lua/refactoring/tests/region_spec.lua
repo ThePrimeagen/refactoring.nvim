1fe17cc985aa1dd26934163b84d278bce751c71c
ThePrimeagen <mpaulson@netflix.com>
2021-08-20 12:27:12 -0600

    feat: region contains

--- lua/refactoring/tests/region_spec.lua
+++ lua/refactoring/tests/region_spec.lua
@@ -41,4 +41,29 @@ describe("Region", function()
         local region = Region:from_current_selection()
         eq({ "(true) {", "    bar" }, region:get_text())
     end)
+
+    it("contain region", function()
+        local region = Region:from_values(0, 10, 100, 12, 50)
+        local ins = {
+            Region:from_values(0, 11, 0, 11, 69),
+            Region:from_values(0, 10, 100, 12, 50),
+            Region:from_values(0, 10, 100, 10, 10000),
+        }
+
+        local outs = {
+            Region:from_values(0, 9, 100, 12, 50), -- out on start row
+            Region:from_values(0, 10, 99, 11, 69), -- out on start col
+            Region:from_values(0, 11, 100, 13, 50), -- out on end row
+            Region:from_values(0, 11, 51, 12, 51), -- out on end col
+            Region:from_values(1, 11, 51, 11, 52), -- diff bufnr
+        }
+
+        for _, v in pairs(ins) do
+            eq(true, region:contains(v))
+        end
+
+        for _, v in pairs(outs) do
+            eq(false, region:contains(v))
+        end
+    end)
 end)
