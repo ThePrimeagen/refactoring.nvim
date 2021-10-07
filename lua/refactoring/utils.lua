fa36258b47d03149cf7060701a9372df3727bb2a
ThePrimeagen <mpaulson@netflix.com>
2021-10-07 13:03:47 -0600

    fix: extract function issue.
    This pr was originally not merged so I cleaned it up and redid it.

--- lua/refactoring/utils.lua
+++ lua/refactoring/utils.lua
@@ -159,6 +159,10 @@ function M.node_text_to_set(...)
         local nodes = select(i, ...)
         for _, node in pairs(nodes) do
             out[ts_utils.get_node_text(node)[1]] = true
+            local text = ts_utils.get_node_text(node)
+            if text and text[1] ~= nil then
+                out[text[1]] = true
+            end
         end
     end
     return out
