e02fc0e390d6952d02b53d2ff1484022d578a97d
ThePrimeagen <mpaulson@netflix.com>
2021-10-27 14:48:01 -0600

    feat: version.  Better versioning.  I think this will be nice.

---
+++ lua/refactoring/version.lua
@@ -0,0 +1,32 @@
+local bits = require("refactoring.bits")
+
+---@class Version
+---@field version number: the flag set
+local Version = {}
+Version.__index = Version
+
+function Version:new(...)
+    local version = 0
+    for i = 1, select("#", ...) do
+        version = bits.bor(version, select(i, ...))
+    end
+
+    return setmetatable({
+        version = version,
+    }, self)
+end
+
+function Version:ensure_version(...)
+    local has_flags = true
+    for i = 1, select("#", ...) do
+        has_flags = has_flags and bits.band(self.version, select(i, ...))
+    end
+
+    if not has_flags then
+        error("This operation isn't supported for this language.")
+    end
+end
+
+return Version
+
+
