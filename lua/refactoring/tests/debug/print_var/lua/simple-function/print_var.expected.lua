ea634a0bc7b585075e59865e10463f298639a77a
teddylear <teddylear@gmail.com>
2021-11-26 13:30:34 -0500

    refactor: change the way print lua varibles for debugging

--- lua/refactoring/tests/debug/print_var/lua/simple-function/print_var.expected.lua
+++ lua/refactoring/tests/debug/print_var/lua/simple-function/print_var.expected.lua
@@ -3,7 +3,7 @@
 function simple_function(a)
     local test = 1
     local test_other = 11
-print(string.format("simple_function test_other: %s", test_other))
+print("simple_function test_other:", test_other)
     for idx = test - 1, test_other do
         print(idx, a)
     end
