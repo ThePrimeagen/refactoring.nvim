e07a39f0869b15db3fc001b2eba4336f7fcc0326
teddylear <teddylear@gmail.com>
2021-12-01 19:54:55 -0500

    fix: Put temporary fix for find-supported-langauges script

--- scripts/find-supported-languages.sh
+++ scripts/find-supported-languages.sh
@@ -1,16 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
-pwd
+languages=(javascript typescript cpp go python lua)
 
-cd queries
-languages=$(find . -mindepth 1 -maxdepth 1 -type d -printf '%f\n')
-cd -
-
-echo $languages
-
-for lang in $languages
+for lang in "${languages[@]}"
 do
-    echo "\nInstalling $lang"
+    echo -e "\nInstalling $lang"
     nvim --headless --clean \
         -u scripts/ci.vim \
         -c "TSInstallSync $lang" -c "q"
