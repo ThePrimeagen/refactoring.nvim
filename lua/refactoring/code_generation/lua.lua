e047eb804e3598047d022f98e9bf23ec85af0753
teddylear <teddylear@gmail.com>
2021-12-03 21:17:11 -0500

    feat: Add ensure code gen to extract func

--- lua/refactoring/code_generation/lua.lua
+++ lua/refactoring/code_generation/lua.lua
@@ -1,4 +1,21 @@
-local utils = require("refactoring.code_generation.utils")
+local code_utils = require("refactoring.code_generation.utils")
+
+local function returnify(args)
+    if type(args) == "string" then
+        return args
+    end
+
+    if #args == 1 then
+        return args[1]
+    end
+
+    local codes = {}
+    for _, value in pairs(args) do
+        table.insert(codes, code_utils.stringify_code(value))
+    end
+
+    return string.format("%s", table.concat(codes, ", "))
+end
 
 local lua = {
     ["print"] = function(print_string)
@@ -20,19 +37,21 @@ end
 ]],
             opts.name,
             table.concat(opts.args, ", "),
-            utils.stringify_code(opts.body)
+            code_utils.stringify_code(opts.body)
         )
     end,
     ["return"] = function(code)
-        return string.format("return %s", utils.stringify_code(code))
+        return string.format("return %s", code_utils.stringify_code(code))
     end,
 
     call_function = function(opts)
         return string.format("%s(%s)", opts.name, table.concat(opts.args, ", "))
     end,
-
     terminate = function(code)
         return code .. "\n"
+    end,
+    pack = function(opts)
+        return returnify(opts)
     end,
 }
 return lua
--- lua/refactoring/code_generation/python.lua
+++ lua/refactoring/code_generation/python.lua
@@ -1,11 +1,28 @@
-local utils = require("refactoring.code_generation.utils")
+local code_utils = require("refactoring.code_generation.utils")
+
+local function returnify(args)
+    if type(args) == "string" then
+        return args
+    end
+
+    if #args == 1 then
+        return args[1]
+    end
+
+    local codes = {}
+    for _, value in pairs(args) do
+        table.insert(codes, code_utils.stringify_code(value))
+    end
+
+    return string.format("%s", table.concat(codes, ", "))
+end
 
 local python = {
     constant = function(opts)
         return string.format("%s = %s\n", opts.name, opts.value)
     end,
     ["return"] = function(code)
-        return string.format("return %s", utils.stringify_code(code))
+        return string.format("return %s", code_utils.stringify_code(code))
     end,
 
     ["function"] = function(opts)
@@ -18,15 +35,17 @@ def %s(%s):
 ]],
             opts.name,
             table.concat(opts.args, ", "),
-            utils.stringify_code(opts.body)
+            code_utils.stringify_code(opts.body)
         )
     end,
     call_function = function(opts)
         return string.format("%s(%s)", opts.name, table.concat(opts.args, ", "))
     end,
-
     terminate = function(code)
         return code .. "\n"
+    end,
+    pack = function(opts)
+        return returnify(opts)
     end,
 }
 return python
