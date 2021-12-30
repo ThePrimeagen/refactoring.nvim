3f90e1e1804455961a76144decc3c32a294f4591
Peter <peter@sabelzabel.nl>
2021-12-30 18:48:58 +0100

    feat: php-debug added

--- lua/refactoring/treesitter/init.lua
+++ lua/refactoring/treesitter/init.lua
@@ -5,6 +5,7 @@ local go = require("refactoring.treesitter.langs.go")
 local Lua = require("refactoring.treesitter.langs.lua")
 local Python = require("refactoring.treesitter.langs.python")
 local JavaScript = require("refactoring.treesitter.langs.javascript")
+local php = require("refactoring.treesitter.langs.php")
 
 local M = {
     TreeSitter = TreeSitter,
@@ -13,6 +14,7 @@ local M = {
     python = Python,
     go = go,
     lua = Lua,
+    php = php,
 
     -- Why so many...
     cc = Cpp,
