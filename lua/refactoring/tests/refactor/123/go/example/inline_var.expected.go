9956a26f3f721303b651d862e756ca1a0c4ff191
Pranav Rao <56097527+pranavrao145@users.noreply.github.com>
2022-01-04 11:48:30 -0500

    test(standardize-refactor-features): added test for golang inline var

---
+++ lua/refactoring/tests/refactor/123/go/example/inline_var.expected.go
@@ -0,0 +1,13 @@
+package main
+
+import "math"
+
+type Order struct {
+	quantity  float64
+	itemPrice float64
+}
+
+func orderCalculation(order Order) float64 {
+	
+	return order.quantity * order.itemPrice - math.Max(0, order.quantity-500)*order.itemPrice*0.05 + math.Min(order.quantity * order.itemPrice*0.1, 100.0)
+}
