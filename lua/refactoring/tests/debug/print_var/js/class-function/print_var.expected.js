7c66c5f12c6fadb140dfa5f98b40e3e858683ec0
Pranav Rao <56097527+pranavrao145@users.noreply.github.com>
2022-01-04 12:34:36 -0500

    test(standardize-debug-features): added debug tests for js

---
+++ lua/refactoring/tests/debug/print_var/js/class-function/print_var.expected.js
@@ -0,0 +1,7 @@
+class Poggers {
+    simple_function() {
+        const i = 3;
+        console.log("Poggers#simple_function i: %s", i);// __AUTO_GENERATED_PRINT_VAR__
+        console.log(i);
+    }
+}
---
+++ lua/refactoring/tests/debug/printf/js/simple-function/printf.expected.js
@@ -0,0 +1,9 @@
+class foo {
+
+    test() {
+        "that seems wrong"
+        console.log("foo#test");// __AUTO_GENERATED_PRINTF__
+        return 5
+    }
+}
+
