b8bb075a11f3a729464510588c7681eaf5faa00a
Pranav Rao <56097527+pranavrao145@users.noreply.github.com>
2022-01-03 20:46:46 -0500

    feat(standardize-refactor-features): standardized treesitter for python

--- lua/refactoring/treesitter/langs/python.lua
+++ lua/refactoring/treesitter/langs/python.lua
@@ -2,6 +2,7 @@ local TreeSitter = require("refactoring.treesitter.treesitter")
 local Version = require("refactoring.version")
 local Nodes = require("refactoring.treesitter.nodes")
 local FieldNode = Nodes.FieldNode
+local InlineNode = Nodes.InlineNode
 
 local Python = {}
 
@@ -16,7 +17,18 @@ function Python.new(bufnr, ft)
         bufnr = bufnr,
         scope_names = {
             function_definition = "function",
-            module = "function",
+            module = "program",
+        },
+        block_scope = {
+            block = true,
+            function_definition = true,
+            module = true,
+        },
+        variable_scope = {
+            assignment = true,
+        },
+        local_var_values = {
+            InlineNode("(assignment right: (_) @tmp_capture)"),
         },
         valid_class_nodes = {
             class_definition = 0,
@@ -24,6 +36,14 @@ function Python.new(bufnr, ft)
         debug_paths = {
             class_definition = FieldNode("name"),
             function_definition = FieldNode("name"),
+        },
+        statements = {
+            InlineNode("(expression_statement) @tmp_capture"),
+            InlineNode("(return_statement) @tmp_capture"),
+            InlineNode("(if_statement) @tmp_capture"),
+            InlineNode("(for_statement) @tmp_capture"),
+            InlineNode("(while_statement) @tmp_capture"),
+            InlineNode("(assignment) @tmp_capture"),
         },
     }, bufnr)
 end
