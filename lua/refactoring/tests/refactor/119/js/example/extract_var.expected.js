a92b9b277016b8495ed41363ae44477cbade9e68
Pranav Rao <56097527+pranavrao145@users.noreply.github.com>
2022-01-04 12:29:27 -0500

    test(standardize-refactor-features): added tests for js extract var

---
+++ lua/refactoring/tests/refactor/119/js/example/extract_var.expected.js
@@ -0,0 +1,12 @@
+// This the straight from the book (slight modifications)
+order.quantity * order.itemPrice;
+function orderCalculation(order) {
+    foo.blah * nonsense.nonsense;
+    const basePrice = order.quantity*order.itemPrice;
+    basePrice;
+    return (
+        basePrice -
+        Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
+        Math.min(basePrice * 0.1, 100)
+    );
+}
