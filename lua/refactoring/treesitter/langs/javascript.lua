c383a9f0dbb3d1c7fb206902b7dce65e42828d79
Pranav Rao <56097527+pranavrao145@users.noreply.github.com>
2022-01-04 12:07:55 -0500

    feat(standardize-refactor-features): standardized treesitter for js

--- lua/refactoring/treesitter/langs/javascript.lua
+++ lua/refactoring/treesitter/langs/javascript.lua
@@ -1,5 +1,9 @@
 local TreeSitter = require("refactoring.treesitter.treesitter")
 local Version = require("refactoring.version")
+local Nodes = require("refactoring.treesitter.nodes")
+local InlineNode = Nodes.InlineNode
+local StringNode = Nodes.StringNode
+local FieldNode = Nodes.FieldNode
 
 local JavaScript = {}
 
@@ -17,6 +21,44 @@ function JavaScript.new(bufnr, ft)
             method_definition = "function",
             arrow_function = "function",
             class_declaration = "class",
+        },
+        block_scope = {
+            statement_block = true,
+            function_declaration = true,
+        },
+        variable_scope = {
+            variable_declaration = true,
+            lexical_declaration = true,
+        },
+        local_var_names = {
+            InlineNode("(variable_declarator name: (_) @tmp_capture)"),
+        },
+        local_var_values = {
+            InlineNode("(variable_declarator value: (_) @tmp_capture)"),
+        },
+        debug_paths = {
+            function_declaration = FieldNode("name"),
+            method_definition = FieldNode("name"),
+            class_declaration = FieldNode("name"),
+            arrow_function = function(node)
+                return FieldNode("name")(node:parent(), "(anon)")
+            end,
+            if_statement = StringNode("if"),
+            for_statement = StringNode("for"),
+            for_in_statement = StringNode("for_in"),
+            while_statement = StringNode("while"),
+            do_statement = StringNode("do"),
+        },
+        statements = {
+            InlineNode("(expression_statement) @tmp_capture"),
+            InlineNode("(return_statement) @tmp_capture"),
+            InlineNode("(if_statement) @tmp_capture"),
+            InlineNode("(for_statement) @tmp_capture"),
+            InlineNode("(for_in_statement) @tmp_capture"),
+            InlineNode("(do_statement) @tmp_capture"),
+            InlineNode("(while_statement) @tmp_capture"),
+            InlineNode("(lexical_declaration) @tmp_capture"),
+            InlineNode("(variable_declaration) @tmp_capture"),
         },
     }, bufnr)
 end
