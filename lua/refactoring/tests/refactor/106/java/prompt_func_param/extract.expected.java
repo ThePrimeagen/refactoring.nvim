effedefa12b7a2050ebba570dc32e491f1508292
Pranav Rao <56097527+pranavrao145@users.noreply.github.com>
2022-01-22 21:55:39 -0500

    test(add-java-support): added 106 tests for java

---
+++ lua/refactoring/tests/refactor/106/java/prompt_func_param/extract.expected.java
@@ -0,0 +1,19 @@
+class Extract {
+
+    public static void foo(int a) {
+        int test_other = 1;
+
+        for (int i = 0; i < test_other; i++) {
+            System.out.printf("%d %d", a, test_other);
+        }
+    }
+
+
+    public static int simpleFunction(int a) {
+        int test = 1;
+        foo(a);
+
+
+        return test;
+    }
+}
---
+++ lua/refactoring/tests/refactor/106/java/prompt_func_param_no_return/extract.start.java
@@ -0,0 +1,11 @@
+class Extract {
+
+  public static void simpleFunction(int a) {
+    int test = 1;
+    int test_other = 1;
+
+    for (int i = 0; i < test_other; i++) {
+      System.out.printf("%d %d", a, test_other);
+    }
+  }
+}
