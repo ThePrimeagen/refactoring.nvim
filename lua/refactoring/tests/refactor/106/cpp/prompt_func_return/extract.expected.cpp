d991db8631740db6944aa67696824337ee825438
Pranav Rao <56097527+pranavrao145@users.noreply.github.com>
2022-02-04 12:50:11 -0500

    test(return-support-cpp-java): added return tests for cpp

---
+++ lua/refactoring/tests/refactor/106/cpp/prompt_func_return/extract.expected.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int foo() {
+      int i = 3;
+  printf("%d", i);
+return i;
+}
+
+
+int simple_function(int a) {
+  printf("this is a test\n");
+auto i = foo();
+
+
+  return i;
+}
---
+++ lua/refactoring/tests/refactor/106/cpp/prompt_func_return/extract.start.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int simple_function(int a) {
+  printf("this is a test\n");
+  int i = 3;
+  printf("%d", i);
+
+  return i;
+}
