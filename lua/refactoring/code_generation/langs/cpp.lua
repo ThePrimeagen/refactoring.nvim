85ec561c4d1b2bab1f0c9b989fbb7dc3517dbd20
Pranav Rao <56097527+pranavrao145@users.noreply.github.com>
2022-02-03 21:56:37 -0500

    fix(return-support-cpp-java): fixed unreturnified code upon extract with return vals

--- lua/refactoring/code_generation/langs/cpp.lua
+++ lua/refactoring/code_generation/langs/cpp.lua
@@ -1,5 +1,7 @@
 local code_utils = require("refactoring.code_generation.utils")
 
+local string_pattern = "%s"
+
 local function cpp_func_args_default_types(args)
     local new_args = {}
     for _, arg in ipairs(args) do
@@ -50,7 +52,7 @@ local function cpp_constant(opts)
     else
         constant_string_pattern = string.format(
             "auto %s = %s;\n",
-            opts.name,
+            code_utils.returnify(opts.name, string_pattern),
             opts.value
         )
     end
