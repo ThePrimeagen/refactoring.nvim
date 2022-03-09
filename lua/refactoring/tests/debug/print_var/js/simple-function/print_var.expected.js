9c2787b27730949ece293869a486e4fbb91ab722
Pranav Rao <56097527+pranavrao145@users.noreply.github.com>
2022-03-09 12:57:56 -0500

    test(remove-auto-formatting): fixed debug tests for removed auto formatting

--- lua/refactoring/tests/debug/print_var/js/simple-function/print_var.expected.js
+++ lua/refactoring/tests/debug/print_var/js/simple-function/print_var.expected.js
@@ -1,5 +1,5 @@
 function simple_function() {
     const i = 3;
-    console.log("simple_function i: %s", i);// __AUTO_GENERATED_PRINT_VAR__
+console.log("simple_function i: %s", i);// __AUTO_GENERATED_PRINT_VAR__
     console.log(i);
 }
--- lua/refactoring/tests/debug/printf/java/class-function/printf.expected.java
+++ lua/refactoring/tests/debug/printf/java/class-function/printf.expected.java
@@ -1,10 +1,10 @@
 class Extract {
 
-    public static void simpleFunction(int a) {
-        int test = 1, test_other = 11;
-        for (int x = 0; x < test_other + test; x++) {
-            System.out.println("Extract#simpleFunction#for");// __AUTO_GENERATED_PRINTF__
-            System.out.println(x + " " + a);
-        }
+  public static void simpleFunction(int a) {
+    int test = 1, test_other = 11;
+    for (int x = 0; x < test_other + test; x++) {
+System.out.println("Extract#simpleFunction#for");// __AUTO_GENERATED_PRINTF__
+      System.out.println(x + " " + a);
     }
+  }
 }
