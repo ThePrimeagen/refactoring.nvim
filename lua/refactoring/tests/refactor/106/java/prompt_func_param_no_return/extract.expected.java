19f1f0f63dca83fe6a191745ae9215224888fddc
Pranav Rao <56097527+pranavrao145@users.noreply.github.com>
2022-03-09 11:42:11 -0500

    test(remove-auto-formatting): fixed refactor tests for removed auto formatting

--- lua/refactoring/tests/refactor/106/java/prompt_func_param_no_return/extract.expected.java
+++ lua/refactoring/tests/refactor/106/java/prompt_func_param_no_return/extract.expected.java
@@ -1,17 +1,17 @@
 class Extract {
 
-    public static void foo(int a) {
-        int test_other = 1;
+public static void foo(int a) {
+    int test_other = 1;
 
-        for (int i = 0; i < test_other; i++) {
-            System.out.printf("%d %d", a, test_other);
-        }
+    for (int i = 0; i < test_other; i++) {
+      System.out.printf("%d %d", a, test_other);
     }
+}
 
 
-    public static void simpleFunction(int a) {
-        int test = 1;
-        foo(a);
+  public static void simpleFunction(int a) {
+    int test = 1;
+    foo(a);
 
-    }
+  }
 }
