f75b16f26f823d13a5cbe7f0ada063a7cecb5dab
Pranav Rao <56097527+pranavrao145@users.noreply.github.com>
2022-04-02 18:54:21 -0400

    test(extract_block): create tests for extract block

---
+++ lua/refactoring/tests/refactor/106/c/extract-block/extract_block.expected.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void foo_bar(INSERT_PARAM_TYPE a) {
+  int test = 1, test_other = 1;
+
+    for (int idx = test - 1; idx < test_other; idx++) {
+      printf("%d %d", idx, a);
+    }
+}
+
+
+void simple_function(int a) {
+  foo_bar(a);
+}
---
+++ lua/refactoring/tests/refactor/106/lua/extract-block/extract_block.expected.lua
@@ -0,0 +1,14 @@
+-- stylua: ignore start
+
+local function foo_bar(a)
+    local test = 1
+        local test_other = 11
+        for idx = test - 1, test_other do
+            print(idx, a)
+        end
+end
+
+
+function simple_function(a)
+    foo_bar(a)
+end
