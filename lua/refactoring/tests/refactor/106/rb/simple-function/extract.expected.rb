c080d91b1f1c4465f0ad560bb9eea2dfb7290ca4
Oli Morris <olimorris@users.noreply.github.com>
2022-05-18 12:30:51 +0100

    complete simple-function refactor

--- lua/refactoring/tests/refactor/106/rb/simple-function/extract.expected.rb
+++ lua/refactoring/tests/refactor/106/rb/simple-function/extract.expected.rb
@@ -1,15 +1,13 @@
 
-def extracted(a, test, test_other)
+def foo_bar(a, test, test_other)
   [test, test_other].each do |v|
     puts "#{a} #{v}"
   end
 end
 
-
 def simple_function(a)
   test = 1
   test_other = 11
 
-  extracted(a, test, test_other)
-
+  foo_bar(a, test, test_other)
 end
--- lua/refactoring/treesitter/langs/ruby.lua
+++ lua/refactoring/treesitter/langs/ruby.lua
@@ -1,44 +1,38 @@
 local TreeSitter = require("refactoring.treesitter.treesitter")
 local Nodes = require("refactoring.treesitter.nodes")
 local FieldNode = Nodes.FieldNode
-local StringNode = Nodes.StringNode
 local InlineNode = Nodes.InlineNode
 
-local ruby = {}
+local Ruby = {}
 
-function ruby.new(bufnr, ft)
+function Ruby.new(bufnr, ft)
     return TreeSitter:new({
         filetype = ft,
         bufnr = bufnr,
         scope_names = {
             program = "program",
-            method_declaration = "def",
-            function_declaration = "def",
             function_definition = "def",
-            class_declaration = "class",
         },
         indent_scopes = {
-            function_declaration = true,
-            function_definition = true,
+            program = true,
         },
-        variable_scope = {
-            variable_declaration = true,
-            local_variable_declaration = false,
+        local_var_names = {
+            InlineNode("(assignment left: (_ (_) @tmp_capture))"),
+            InlineNode("(assignment left: (_) @tmp_capture)"),
         },
         function_args = {
             InlineNode(
                 "((method (method_parameters (identifier) @tmp_capture)))"
             ),
         },
-        local_var_names = {
-            InlineNode(
-                "( assignment (identifier) @definition.local_name)"
-            ),
+        local_var_values = {
+            InlineNode("(assignment right: (_ (_) @tmp_capture))"),
+            InlineNode("(assignment right: (_) @tmp_capture)"),
         },
         local_declarations = {
-            InlineNode("(assignment) @tmp_capture"),
+            InlineNode("((assignment) @tmp_capture)"),
         },
     }, bufnr)
 end
 
-return ruby
+return Ruby
