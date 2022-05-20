8a1560eedfc3aea7afd342ecd59de307233024e1
Oli Morris <olimorris@users.noreply.github.com>
2022-05-20 13:19:39 +0100

    wip: ruby extract class method

--- lua/refactoring/treesitter/langs/ruby.lua
+++ lua/refactoring/treesitter/langs/ruby.lua
@@ -10,27 +10,41 @@ function Ruby.new(bufnr, ft)
         filetype = ft,
         bufnr = bufnr,
         scope_names = {
-            program = "program",
-            function_definition = "def",
+            module = "program",
+            method = "function",
         },
         indent_scopes = {
-            program = true,
+            method = true,
+            module = true,
         },
         local_var_names = {
-            InlineNode("(assignment left: (_ (_) @tmp_capture))"),
             InlineNode("(assignment left: (_) @tmp_capture)"),
+        },
+        local_var_values = {
+            InlineNode("(assignment right: (_) @tmp_capture)"),
+        },
+        function_scopes = {
+            method = "function",
         },
         function_args = {
             InlineNode(
-                "((method (method_parameters (identifier) @tmp_capture)))"
+                "(method parameters: (method_parameters (identifier) @tmp_capture))"
             ),
         },
-        local_var_values = {
-            InlineNode("(assignment right: (_ (_) @tmp_capture))"),
+        function_body = {
+            InlineNode("(assignment left: (_) @tmp_capture)"),
             InlineNode("(assignment right: (_) @tmp_capture)"),
         },
         local_declarations = {
             InlineNode("((assignment) @tmp_capture)"),
+        },
+        class_names = {
+            InlineNode(
+                "(class name: (constant) @tmp_capture)"
+            ),
+        },
+        valid_class_nodes = {
+            class = 0,
         },
     }, bufnr)
 end
