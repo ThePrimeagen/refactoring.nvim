6a0399d54ec22f36b342ac1488e4ab59de632a5a
Oli Morris <olimorris@users.noreply.github.com>
2022-05-25 15:34:27 +0100

    add back with-comment tests

---
+++ lua/refactoring/tests/refactor/106/rb/with-comments/extract.start.rb
@@ -0,0 +1,11 @@
+# This is a comment
+# Ruby still rocks
+# It's not dead
+def simple_function(a)
+  test = 1
+  test_other = 11
+
+  [test, test_other].each do |v|
+    puts "#{a} #{v}"
+  end
+end
