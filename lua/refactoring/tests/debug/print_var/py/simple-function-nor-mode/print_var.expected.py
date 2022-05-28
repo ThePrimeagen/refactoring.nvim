e0e6d03ada0c65083a51b3a5f4b350d1b45e3590
teddylear <teddylear@gmail.com>
2022-05-28 13:13:22 -0400

    fix: update indenting on print_var tests for normal mode

--- lua/refactoring/tests/debug/print_var/py/simple-function-nor-mode/print_var.expected.py
+++ lua/refactoring/tests/debug/print_var/py/simple-function-nor-mode/print_var.expected.py
@@ -2,7 +2,7 @@ class Poggers:
 
     def simple_function(self, a):
         test = 1
-print(f"Poggers#simple_function test: {str(test)}")# __AUTO_GENERATED_PRINT_VAR__
+        print(f"Poggers#simple_function test: {str(test)}")# __AUTO_GENERATED_PRINT_VAR__
         test_other = 11
         for x in range(test_other + test):
             print(x, a)
