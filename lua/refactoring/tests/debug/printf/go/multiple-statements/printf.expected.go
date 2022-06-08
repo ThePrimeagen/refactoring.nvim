15adce14c5145e3b53902cf9ec48f572f592f49b
teddylear <teddylear@gmail.com>
2022-06-07 21:44:48 -0400

    fixing tests

--- lua/refactoring/tests/debug/printf/go/multiple-statements/printf.expected.go
+++ lua/refactoring/tests/debug/printf/go/multiple-statements/printf.expected.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func main()  {
+func main() {
     fmt.Println("test")
-    fmt.Println("debug path main")// __AUTO_GENERATED_PRINTF__
+	fmt.Println("debug path main")// __AUTO_GENERATED_PRINTF__
 }
