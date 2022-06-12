f5f00dd86567dad1644befc0b3cfc2d4d5b023f8
teddylear <teddylear@gmail.com>
2022-06-12 19:17:01 -0400

    feat: Split print_var and print_var_norm behaviors so we can iterate on print_var_norm

--- lua/refactoring/debug/init.lua
+++ lua/refactoring/debug/init.lua
@@ -1,6 +1,7 @@
 local Config = require("refactoring.config")
 local printf = require("refactoring.debug.printf")
 local print_var = require("refactoring.debug.print_var")
+local print_var_norm = require("refactoring.debug.print_var_norm")
 local get_path = require("refactoring.debug.get_path")
 local cleanup = require("refactoring.debug.cleanup")
 
@@ -14,6 +15,11 @@ end
 function M.print_var(opts)
     local config = Config.get():merge(opts)
     return print_var(vim.api.nvim_get_current_buf(), config)
+end
+
+function M.print_var_norm(opts)
+    local config = Config.get():merge(opts)
+    return print_var_norm(vim.api.nvim_get_current_buf(), config)
 end
 
 function M.cleanup(opts)
