c011a98d2bda5d5fc8243ee383fb6afe0bad5e4d
Oli Morris <olimorris@users.noreply.github.com>
2022-06-27 19:48:00 +0100

    feat: 119 extract variable

---
+++ lua/refactoring/tests/refactor/119/rb/example/extract_var.expected.rb
@@ -0,0 +1,9 @@
+def orderCalculation(order, a, b)
+    temp = a * b
+  basePrice = order.quantity*order.itemPrice
+   blah = (basePrice) - 7
+
+    puts(temp, blah)
+
+    basePrice - max(0, order.quantity - 500) * order.itemPrice * 0.05 + min(basePrice * 0.01, 100)
+end
