7dd4c416c2d3898c05a4f5eec0fcd176da3e0db5
Oli Morris <olimorris@users.noreply.github.com>
2022-06-27 22:32:33 +0100

    feat: add debug cleanup tests

---
+++ lua/refactoring/tests/debug/cleanup/rb/cleanup.start.rb
@@ -0,0 +1,7 @@
+puts('test')  # __AUTO_GENERATED_PRINTF__
+def main
+  puts('main') # __AUTO_GENERATED_PRINTF__
+  puts('ruby is awesome')
+  i = 3
+  puts(' i: {str(i)}')  # __AUTO_GENERATED_PRINT_VAR__
+end
