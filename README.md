b3862e895156dde43753440523c414d6f9f7f096
Wuilliam Lacruz <wuilliam.lacruz@mercadolibre.com>
2023-06-15 19:38:43 -0300

    last comments (for now)

--- README.md
+++ README.md
@@ -167,6 +167,7 @@ vim.api.nvim_set_keymap("v", "<leader>re", [[ <Esc><Cmd>lua require('refactoring
 vim.api.nvim_set_keymap("v", "<leader>rf", [[ <Esc><Cmd>lua require('refactoring').refactor('Extract Function To File')<CR>]], {noremap = true, silent = true, expr = false})
 vim.api.nvim_set_keymap("v", "<leader>rv", [[ <Esc><Cmd>lua require('refactoring').refactor('Extract Variable')<CR>]], {noremap = true, silent = true, expr = false})
 vim.api.nvim_set_keymap("v", "<leader>ri", [[ <Esc><Cmd>lua require('refactoring').refactor('Inline Variable')<CR>]], {noremap = true, silent = true, expr = false})
+vim.api.nvim_set_keymap("v", "<leader>rif", [[ <Esc><Cmd>lua require('refactoring').refactor('Inline Function')<CR>]], {noremap = true, silent = true, expr = false})
 
 -- Extract block doesn't need visual mode
 vim.api.nvim_set_keymap("n", "<leader>rb", [[ <Cmd>lua require('refactoring').refactor('Extract Block')<CR>]], {noremap = true, silent = true, expr = false})
--- lua/refactoring/refactor/115.lua
+++ lua/refactoring/refactor/115.lua
@@ -8,48 +8,10 @@ local utils = require("refactoring.utils")
 local indent = require("refactoring.indent")
 local code = require("refactoring.code_generation")
 
-local function dump(o)
-    if type(o) == "table" then
-        local s = "{ "
-        for k, v in pairs(o) do
-            if type(k) ~= "number" then
-                k = '"' .. k .. '"'
-            end
-            s = s .. "[" .. k .. "] = " .. dump(v) .. ","
-        end
-        return s .. "} "
-    else
-        return tostring(o)
-    end
-end
-
-local function dumpcode(o)
-    if type(o) == "table" then
-        local out = {}
-        for idx, identifier in pairs(o) do
-            local region = Region:from_node(identifier)
-            local parameter_list = region:get_text()
-            table.insert(out, idx, parameter_list)
-        end
-        return dump(out)
-    else
-        local region = Region:from_node(o)
-        local parameter_list = region:get_text()
-        return dump(parameter_list)
-    end
-end
-
-local function pdebug(o)
-    print("INLINE_FUNC_DEBUG")
-    print(dump(o))
-    print("")
-end
-
 local M = {}
 
 local function get_inline_setup_pipeline(bufnr, opts)
     return Pipeline:from_task(refactor_setup(bufnr, opts))
-    -- :add_task(selection_setup)
 end
 
 local function get_function_declaration(refactor, bufnr)
@@ -91,15 +53,6 @@ local function get_function_parameter_names(refactor, function_declaration, bufn
     return values
 end
 
-local function get_function_receiver_names(refactor, function_declaration, bufnr)
-    -- TODO: multiple return points are not supported
-    local values = {}
-    for _, value in ipairs(refactor.ts:loop_thru_nodes(function_declaration:parent(), refactor.ts.local_var_names)) do
-        table.insert(values, vim.treesitter.get_node_text(value, bufnr))
-    end
-    return values
-end
-
 local function get_function_arguments(refactor, declarator_node, bufnr)
     local args = {}
     for _, value in ipairs(refactor.ts:loop_thru_nodes(declarator_node, refactor.ts.caller_args)) do
@@ -123,7 +76,7 @@ local function get_function_body_text(refactor, function_declaration, bufnr)
 
     -- update the region and its node with the block scope found
     local region = Region:from_values(
-        refactor.bufnr,
+        bufnr,
         first_line_region.start_row,
         -- The Tresitter delimited region never includes the blank spaces
         -- before the first line which causes problems with indentation.
@@ -153,18 +106,6 @@ local function get_function_body(refactor, function_declaration, bufnr)
         end
     end
     return function_body
-end
-
-local function get_params_as_declarations(refactor, keys, values)
-    -- TODO: keys length and values should be the same
-    local var_declarations = {}
-    for idx, _ in ipairs(keys) do
-        table.insert(var_declarations, refactor.code.var_declaration({
-            name = keys[idx],
-            value = values[idx],
-        }))
-    end
-    return var_declarations
 end
 
 local function get_indent_spaces(refactor, node, bufnr)
@@ -197,186 +138,8 @@ local function get_params_as_constants(refactor, indent_space, keys, values)
     return constants
 end
 
--- local function get_receivers_as_constants(refactor, receiver_names, values)
---     -- TODO: keys length and values should be the same
---     local constants = {}
---     for idx, _ in ipairs(receiver_names) do
---         table.insert(constants, refactor.code.constant({
---             name = receiver_names[idx],
---             value = values[idx],
---         }))
---     end
---     local lastElement = constants[#constants]
---     table.remove(constants, #constants)
---     table.insert(constants, 1, lastElement)
---     return constants
--- end
-
-local function inlined_sentences_edits(refactor, region, bufnr)
-    local text_edits = {}
-    local declaration, _ = get_function_declaration(refactor, bufnr)
-    local function_declaration_body = get_function_body(refactor, declaration, bufnr)
-    for _, sentence in ipairs(function_declaration_body) do
-        local text_edit = lsp_utils.insert_text(region, sentence)
-        table.insert(text_edits, text_edit)
-    end
-    return text_edits
-end
-
-
-local function delete_region(region)
-    local text_edits = {}
-    local delete_text = lsp_utils.delete_text(region)
-    table.insert(text_edits, delete_text)
-    return text_edits
-end
-
 local function inline_func_setup(refactor, bufnr)
-    local text_edits                       = {}
-    local function_declaration, identifier = get_function_declaration(refactor, bufnr)
-    local function_references              = get_references(refactor, function_declaration, identifier, bufnr)
-
-    if #function_references == 0 then
-        error("Error: no function usages to inline")
-        return false, refactor
-    end
-
-    for _, reference in ipairs(function_references) do
-        -- TODO: multiple return points are not supported
-        local reference_receivers_names = get_function_receiver_names(refactor, reference:parent():parent():parent(),
-            bufnr)
-        if #reference_receivers_names == 0 then
-            local returned_values = get_function_returned_values(refactor, function_declaration, bufnr)
-            if #returned_values == 1 then
-                -- inlines function body into the new place (without return statements)
-                for _, rv in ipairs(returned_values) do
-                    local insert_text = lsp_utils.insert_text(Region:from_node(reference:parent(), bufnr), rv)
-                    table.insert(text_edits, insert_text)
-                end
-
-                -- deletes the original reference
-                for _, edit in ipairs(delete_region(Region:from_node(reference:parent(), bufnr))) do
-                    table.insert(text_edits, edit)
-                end
-            end
-        end
-        if #reference_receivers_names == 1 then
-            local returned_values = get_function_returned_values(refactor, function_declaration, bufnr)
-            if #returned_values == 1 then
-                -- inlines function body into the new place (without return statements)
-                for _, rv in ipairs(returned_values) do
-                    local insert_text = lsp_utils.insert_text(Region:from_node(reference, bufnr), rv)
-                    table.insert(text_edits, insert_text)
-                end
-
-                -- deletes the original reference
-                for _, edit in ipairs(delete_region(Region:from_node(reference, bufnr))) do
-                    table.insert(text_edits, edit)
-                end
-            end
-        end
-
-        if #reference_receivers_names > 0 then
-            local reference_region = utils.region_one_line_up_from_node(reference)
-            local returned_values = get_function_returned_values(refactor, function_declaration, bufnr)
-
-            if #returned_values == 1 then
-                -- inlines function body into the new place (without return statements)
-                for _, rv in ipairs(returned_values) do
-                    local insert_text = lsp_utils.insert_text(Region:from_node(reference, bufnr), rv)
-                    table.insert(text_edits, insert_text)
-                end
-
-                -- deletes the original reference
-                for _, edit in ipairs(delete_region(Region:from_node(reference, bufnr))) do
-                    table.insert(text_edits, edit)
-                end
-            else
-                -- write the var declarations if there are more than 2 return statemetes in the body
-                if #returned_values > 2 then
-                    local returned_values_types = {}
-                    for _, name in ipairs(reference_receivers_names) do
-                        -- TODO: ask for types or automatically detect them (is that possible?)
-                        table.insert(returned_values_types, name .. "_type")
-                    end
-                    local declarations = get_params_as_declarations(refactor, reference_receivers_names,
-                        returned_values_types)
-                    for _, declaration in ipairs(declarations) do
-                        local insert_text = lsp_utils.insert_text(reference_region, declaration)
-                        table.insert(text_edits, insert_text)
-                    end
-                end
-
-                -- rewrites each parameter with its value in the new place
-                local parameter_names = get_function_parameter_names(refactor, function_declaration, bufnr)
-                local argument_values = get_function_arguments(refactor, reference, bufnr)
-                local constants = get_params_as_constants(refactor, reference:parent(), parameter_names, argument_values)
-                for _, constant in ipairs(constants) do
-                    local insert_text = lsp_utils.insert_text(reference_region, constant)
-                    table.insert(text_edits, insert_text)
-                end
-
-                -- rewrites returned values into constants with its proper names
-                constants = get_params_as_constants(refactor, reference_receivers_names, returned_values)
-                -- constants = get_receivers_as_constants(refactor, reference_receivers_names, returned_values2)
-                for _, constant in ipairs(constants) do
-                    local insert_text = lsp_utils.insert_text(reference_region, constant)
-                    table.insert(text_edits, insert_text)
-                end
-
-                -- inlines function body into the new place (without return statements)
-                for _, edit in ipairs(inlined_sentences_edits(refactor, reference_region, bufnr)) do
-                    table.insert(text_edits, edit)
-                end
-
-                -- deletes the original reference
-                for _, edit in ipairs(delete_region(Region:from_node(reference:parent():parent():parent(), bufnr))) do
-                    table.insert(text_edits, edit)
-                end
-            end
-        else
-            local reference_region = Region:from_node(reference:parent(), bufnr)
-
-            -- local returned_values = get_function_returned_values(refactor, function_declaration, bufnr)
-            -- if #returned_values > 0 then
-            --     -- inlines function body into the new place (without return statements)
-            --     for _, rv in ipairs(returned_values) do
-            --         local insert_text = lsp_utils.insert_text(reference_region, "(" .. rv .. ")")
-            --         table.insert(text_edits, insert_text)
-            --     end
-            -- end
-
-            -- rewrites each parameter with its value in the new place
-            local parameter_names = get_function_parameter_names(refactor, function_declaration, bufnr)
-            local argument_values = get_function_arguments(refactor, reference, bufnr)
-            local constants = get_params_as_constants(refactor, parameter_names, argument_values)
-            for _, constant in ipairs(constants) do
-                local insert_text = lsp_utils.insert_text(reference_region, constant)
-                table.insert(text_edits, insert_text)
-            end
-
-            -- inlines function body into the new place (without return statements)
-            for _, edit in ipairs(inlined_sentences_edits(refactor, reference_region, bufnr)) do
-                table.insert(text_edits, edit)
-            end
-
-            -- deletes the original reference
-            for _, edit in ipairs(delete_region(reference_region)) do
-                table.insert(text_edits, edit)
-            end
-        end
-    end
-
-    -- deletes function declaration
-    local function_declaration_region = Region:from_node(function_declaration, bufnr)
-    table.insert(text_edits, lsp_utils.delete_text(function_declaration_region))
-
-    refactor.text_edits = text_edits
-
-    return true, refactor
-end
-
-local function inline_func_setup_v2(refactor, bufnr)
+    -- TODO: need to get function declaration no matter if we are at the definition or at any reference
     local function_declaration, identifier = get_function_declaration(refactor, bufnr)
     local function_references              = get_references(refactor, function_declaration:parent(), identifier, bufnr)
 
@@ -410,7 +173,7 @@ local function inline_func_setup_v2(refactor, bufnr)
         end
 
         -- replaces the function call with the returned value
-        -- TODO: this could be merge into next one
+        -- TODO: this could be merged into next one
         if #parameters_list == 0 and #returned_values == 1 and #function_declaration_body == 0 then
             refactor_is_possible = true
             for _, sentence in ipairs(returned_values) do
@@ -434,7 +197,7 @@ local function inline_func_setup_v2(refactor, bufnr)
         end
 
         -- replaces the function call with the returned values (multiple)
-        -- TODO: this could be mergen into the next one
+        -- TODO: this could be merged into the next one
         if #parameters_list == 0 and #returned_values > 1 and #function_declaration_body == 0 then
             refactor_is_possible = true
             for idx, sentence in ipairs(returned_values) do
@@ -512,7 +275,7 @@ local function inline_func_setup_v2(refactor, bufnr)
             local new_line = code.new_line()
             -- TODO: a really tricy hack because function_declaration_body contains duplicated nodes that we don't want here
             -- TODO: need to try to reproduce this hack in the other implementations, nested of nested and comments inside of nested
-            -- TODO: this code is not indented
+            -- TODO: this code is not indented for now
             local function_body = function_declaration_body
             if #function_declaration_body ~= #function_declaration_text then
                 function_body = function_declaration_text
@@ -571,7 +334,7 @@ function M.inline_func(bufnr, opts)
         :add_task(
         --- @param refactor Refactor
             function(refactor)
-                return inline_func_setup_v2(refactor, bufnr)
+                return inline_func_setup(refactor, bufnr)
             end
         )
         :after(post_refactor.post_refactor)
