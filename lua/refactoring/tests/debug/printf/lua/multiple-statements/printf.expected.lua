6bae0cbad3a15a02b5265223708c903a4406456c
TheLeoP <eugenio2305@hotmail.com>
2024-01-25 11:44:46 -0500

    test(mutiple-statements lua): use same delimiters as custom statement

--- lua/refactoring/tests/debug/printf/lua/multiple-statements/printf.expected.lua
+++ lua/refactoring/tests/debug/printf/lua/multiple-statements/printf.expected.lua
@@ -2,5 +2,5 @@
 local hello = function()
     print("test")
     -- __AUTO_GENERATED_PRINTF_START__
-    print([==[debug path function 1]==]); -- __AUTO_GENERATED_PRINTF_END__
+    print("debug path function 1"); -- __AUTO_GENERATED_PRINTF_END__
 end
