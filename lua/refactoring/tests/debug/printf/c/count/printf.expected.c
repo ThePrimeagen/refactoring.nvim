4f9f78c4b9e7cb6e063d6950f5c9092f5c9381d9
TheLeoP <eugenio2305@hotmail.com>
2024-01-26 09:35:22 -0500

    test(print_f): lua and c

---
+++ lua/refactoring/tests/debug/printf/c/count/printf.expected.c
@@ -0,0 +1,8 @@
+int main() {
+    // __AUTO_GENERATED_PRINTF_START__
+    printf("main 1(%d): \n", __LINE__); // __AUTO_GENERATED_PRINTF_END__
+    return 0;
+    // __AUTO_GENERATED_PRINTF_START__
+    printf("main 2(%d): \n", __LINE__); // __AUTO_GENERATED_PRINTF_END__
+}
+
---
+++ lua/refactoring/tests/debug/printf/c/count/printf.start.c
@@ -0,0 +1,6 @@
+int main() {
+    return 0;
+    // __AUTO_GENERATED_PRINTF_START__
+    printf("main 1(%d): \n", __LINE__); // __AUTO_GENERATED_PRINTF_END__
+}
+
