088e59fd466993207c98fa6a192a13209a0aa1b6
TheLeoP <eugenio2305@hotmail.com>
2024-02-05 11:28:40 -0500

    fix(106): not extract above imports
    This needs support for other languages besides python

---
+++ lua/refactoring/tests/refactor/106/py/with-imports/extract.start.py
@@ -0,0 +1,29 @@
+import json
+
+def foo():
+    bad_doc_ids = []
+
+    with open("./some/path/full_bad.jsonl", 'r', encoding='utf-8') as f:
+        while True:
+            id = f.readline().strip().strip('"')
+            if not id:
+                break
+            bad_doc_ids.append(id)
+    return bad_doc_ids, id
+
+
+
+if __name__ == "main":
+    bad_doc_ids, id = foo()
+
+
+    bad_docs = []
+    with open("./some/path/full.jsonl", 'r', encoding='utf-8') as f:
+        while True:
+            line = f.readline()
+            if not line:
+                break
+            doc = json.loads(line)
+
+            if doc['id'] in bad_doc_ids:
+                bad_docs.append(id)
