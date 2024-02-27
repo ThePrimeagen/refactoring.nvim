7d0b0e237edfedb0b36a7c013908af50146a9f70
TheLeoP <eugenio2305@hotmail.com>
2024-02-27 07:56:21 -0500

    feat(115): add support for python

---
+++ lua/refactoring/tests/refactor/115/python/example/inline_func.expected.py
@@ -0,0 +1,11 @@
+
+
+
+def simple_function(a):
+    test = 1
+    test_other =     test_other = 11
+    for idx in range(test - 1, test_other):
+        print(idx, a)
+
+
+    return test, test_other
