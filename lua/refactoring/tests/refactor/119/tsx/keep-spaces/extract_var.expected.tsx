9fad9b067121f48730e1a3c4cde702d4722146fc
TheLeoP <eugenio2305@hotmail.com>
2024-07-18 16:45:07 -0500

    fix(tsx): update tsx tests

--- lua/refactoring/tests/refactor/119/tsx/keep-spaces/extract_var.expected.tsx
+++ lua/refactoring/tests/refactor/119/tsx/keep-spaces/extract_var.expected.tsx
@@ -1,10 +1,10 @@
 function foo(name: string) {
-    const foo = <a download href="/hello.jpg">
+    const foo = 
+            <a download href="/hello.jpg">
                 download
             </a>;
     return (
-        <div>
-            {foo}
+        <div>{foo}
         </div>
     );
 }
