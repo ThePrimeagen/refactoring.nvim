835fc117e04dd3d6f6b2c69b805cee0d5ef04d83
TheLeoP <eugenio2305@hotmail.com>
2024-07-18 19:40:58 -0500

    fix(123-python): add filter for references
    Fixes #451

--- lua/refactoring/treesitter/langs/python.lua
+++ lua/refactoring/treesitter/langs/python.lua
@@ -121,6 +121,19 @@ function Python.new(bufnr, ft)
                 "return "
             )
         end,
+        reference_filter = function(node)
+            local parent = node:parent()
+            if not parent then
+                return true
+            end
+            if parent:type() == "default_parameter" then
+                return false
+            end
+            if parent:type() == "keyword_argument" then
+                return parent:field("value")[1] == node
+            end
+            return true
+        end,
         include_end_of_line = true,
     }
     local ts = TreeSitter:new(config, bufnr)
