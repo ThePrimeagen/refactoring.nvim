07370545733004f8f700a358f7a4b23e8795cb75
TheLeoP <eugenio2305@hotmail.com>
2024-07-28 18:31:08 -0500

    fix(lua-123): add filter to handle `{foo = foo}`

--- lua/refactoring/treesitter/langs/lua.lua
+++ lua/refactoring/treesitter/langs/lua.lua
@@ -124,8 +124,14 @@ function Lua.new(bufnr, ft)
             if not parent then
                 return true
             end
+            -- foo.foo
+            -- ^
             if parent:type() == "dot_index_expression" then
                 return parent:field("table")[1] == node
+            -- {foo = foo}
+            --        ^
+            elseif parent:type() == "field" then
+                return parent:field("value")[1] == node
             end
             return true
         end,
