56d856a828b95118a2afe714140663cd5e08695a
TheLeoP <eugenio2305@hotmail.com>
2025-02-19 21:38:39 -0500

    feat(print_var): smarter parent selection js

--- lua/refactoring/treesitter/langs/javascript.lua
+++ lua/refactoring/treesitter/langs/javascript.lua
@@ -93,6 +93,27 @@ function JavaScript.new(bufnr, ft)
 
             InlineNode("(arrow_function (statement_block (_) @tmp_capture))"),
         },
+        should_check_parent_node_print_var = function(node)
+            local parent = node:parent()
+            if not parent then
+                return false
+            end
+            local parent_type = parent:type()
+            if parent_type == "member_expression" then
+                local property_node = parent:field("property")[1]
+                if not property_node then
+                    return false
+                end
+                if not property_node:equal(node) then
+                    return false
+                end
+                return true
+            elseif parent_type == "call_expression" then
+                return true
+            else
+                return false
+            end
+        end,
     }
     return TreeSitter:new(config, bufnr)
 end
