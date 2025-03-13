74c0b11ff6c25807105435739d7e656e5647600b
Domrev Igor <domrevigor@gmail.com>
2025-03-13 04:28:16 +0200

    Update refactoring.lua

--- lua/telescope/_extensions/refactoring.lua
+++ lua/telescope/_extensions/refactoring.lua
@@ -4,7 +4,7 @@ local function refactor(prompt_bufnr)
     local content =
         require("telescope.actions.state").get_selected_entry(prompt_bufnr)
     require("telescope.actions").close(prompt_bufnr)
-    refactoring.refactor(content.value)
+    vim.api.nvim_input(refactoring.refactor(content.value))
 end
 
 local function telescope_refactoring(opts)
