b00a68cebad6baa5eb5fb058b4ecf6e660b67c3d
TheLeoP <eugenio2305@hotmail.com>
2025-03-27 12:54:27 -0500

    fix: make telescope extension work again

--- lua/telescope/_extensions/refactoring.lua
+++ lua/telescope/_extensions/refactoring.lua
@@ -1,24 +1,24 @@
 local refactoring = require("refactoring")
+local actions = require("telescope.actions")
+local action_state = require("telescope.actions.state")
 
 local function refactor(prompt_bufnr)
-    local content =
-        require("telescope.actions.state").get_selected_entry(prompt_bufnr)
-    require("telescope.actions").close(prompt_bufnr)
-    local keys = refactoring.refactor(content.value)
+    actions.close(prompt_bufnr)
+    local content = action_state.get_selected_entry()
 
-    if keys == "g@" then
-        keys = "gvg@"
-    end
-    vim.cmd.normal(keys)
+    -- NOTE: telescope leaves Neovim in insert mode after exiting and even trying to change into normal mode doesn't work
+    -- Is this a bug with telescope? Or with Neovim input buffers?
+    vim.schedule(function()
+        local keys = refactoring.refactor(content.value)
+        if keys == "g@" then
+            keys = "gvg@"
+        end
+        vim.cmd.normal(keys)
+    end)
 end
 
 local function telescope_refactoring(opts)
     opts = opts or require("telescope.themes").get_cursor()
-
-    local utils = require("refactoring.utils")
-    if utils.is_visual_mode() then
-        utils.exit_to_normal_mode()
-    end
 
     require("telescope.pickers")
         .new(opts, {
@@ -27,9 +27,8 @@ local function telescope_refactoring(opts)
                 results = refactoring.get_refactors(),
             }),
             sorter = require("telescope.config").values.generic_sorter(opts),
-            attach_mappings = function(_, map)
-                map("i", "<CR>", refactor)
-                map("n", "<CR>", refactor)
+            attach_mappings = function()
+                actions.select_default:replace(refactor)
                 return true
             end,
         })
