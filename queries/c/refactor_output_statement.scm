8bcf893eb3871bc87b42adffe2fb9b0b7dc9d328
TheLeoP <eugenio2305@hotmail.com>
2026-09-17 13:47:02 -0500

    feat(c): improve output_statement queries

--- queries/c/refactor_output_statement.scm
+++ queries/c/refactor_output_statement.scm
@@ -1,5 +1,39 @@
 [
-  (statement)
-  (function_definition)
+  (attributed_statement)
+  (break_statement)
+  (case_statement)
+  (continue_statement)
+  (expression_statement)
+  (goto_statement)
+  (labeled_statement)
+  (return_statement)
+  (seh_leave_statement)
+  (seh_try_statement)
+  (switch_statement)
   (declaration)
 ] @output_statement
+
+(if_statement
+  consequence: (compound_statement
+    .
+    (_) @output_statement.inside)) @output_statement
+
+(do_statement
+  body: (compound_statement
+    .
+    (_) @output_statement.inside)) @output_statement
+
+(while_statement
+  body: (compound_statement
+    .
+    (_) @output_statement.inside)) @output_statement
+
+(for_statement
+  body: (compound_statement
+    .
+    (_) @output_statement.inside)) @output_statement
+
+(function_definition
+  body: (compound_statement
+    .
+    (_) @output_statement.inside)) @output_statement
